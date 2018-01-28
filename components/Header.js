6e511798c2063d057c8c90c79c8a60a0b9785581
Gregory Kennedy <peacedude@gmail.com>
2018-01-27 19:46:56 -0800

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Menu } from 'semantic-ui-react';
+
+export default () => {
+  return (
+    <Menu style={{ marginTop: '10px' }}>
+    <Menu.Item>WIRDCoin</Menu.Item>
+
+
+
+    <Menu.Menu position="right">
+      <Menu.Item>Campaigns</Menu.Item>
+
+      <Menu.Item>+</Menu.Item>
+    </Menu.Menu>
+    </Menu>
+  );
+};
