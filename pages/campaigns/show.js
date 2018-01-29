31622c163502c3cba53bfeebef10a98b5876455c
Gregory Kennedy <peacedude@gmail.com>
2018-01-29 09:57:18 -0800

    Add files via upload

---
+++ pages/campaigns/show.js
@@ -0,0 +1,82 @@
+import React, { Component } 'react';
+import { Card } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
+
+
+class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.address);
+
+    const summary = await campaign.methods.getSummary().call();
+
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4]
+    };
+  }
+
+renderCards() {
+const {
+  balance,
+  manager,
+  minimumContribution,
+  requestsCount,
+  approversCount
+} = this.props;
+
+  const items = [
+    {
+      header: manager,
+      meta: 'Address of Manager',
+      description:
+      'The manager created this campaign and can create requests to withdraw money.',
+    style: {overflowWrap: 'break-word' }
+    },
+    {
+    header: minimumContribution,
+    meta: 'Minimum Contribution (wei)',
+    description:
+    'You must contribute at least this much wei to become an approver.',
+    },
+    {
+    header: requestsCount,
+    meta: 'Number of Requests',
+    description:
+    'A request tries to withdraw money from the contract. Requests must be approved by approvers.',
+    },
+    {
+    header: approversCount,
+    meta: 'Number of Approvers',
+    description:'Number of people who has already donated to campaign.'
+    },
+    {
+      header: web3.utils.fromWei(balance, 'ether'),
+      meta: 'Campaign Balance (ether)',
+      description:
+      'How much money this campaign has left to spend.'
+    }
+  ];
+
+  return <Card.Group items={items} />;
+}
+
+  render() {
+    return (
+      <Layout>
+        <h3>Campaign Show</h3>
+        {this.renderCards()}
+        <ContributeForm />
+      </Layout>
+    );
+  }
+}
+
+
+
+export default CampaignShow;
