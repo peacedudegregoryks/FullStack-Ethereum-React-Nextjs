cdda7f4e33122689a81191dea1432bd23a26e68b
Gregory Kennedy <peacedude@gmail.com>
2018-01-29 10:03:27 -0800

    Add files via upload

---
+++ components/ContributeForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Form, Input, Message } from 'semantic-ui-react';
+
+class ContributeForm extends Component {
+render() {
+  return(
+    <Form>
+      <Form.Field>
+      <label>Amount to Contribute</label>
+      <Input label="ether" labelPosition="right" />
+        </Form.Field>
+      <Button primary>Contribute!</Button>
+    </Form>
+  );
+ }
+}
