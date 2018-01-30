a371d5f4eb6996ab59c12cd578ae67c78badf168
Gregory Kennedy <peacedude@gmail.com>
2018-01-30 15:09:46 -0800

    Add files via upload

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x6f45a57a772cDC9Debf41fC17547758FFc0E8E97'
+  '0xDDB0c951F9E961bD6C1D8be0DBD31f711c0084Dc'
 );
 
 export default instance;
