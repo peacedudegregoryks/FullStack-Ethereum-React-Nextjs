3ea41093b37a397581abc28753e9e72797cc6828
Gregory Kennedy <peacedude@gmail.com>
2018-01-30 09:34:21 -0800

    Add files via upload

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
 
-    return { address };
+    const request = await Promise.all(
+        Array(parseInt(requestCount)).fill().map((element, index) => {
+        return campaign.methods.requests(index).call();
+      })
+    );
+
+    return { address, requests, requestCount };
   }
 
   render() {
+    const {Header, Row, HeaderCell. Body } = Table;
+
     return (
       <Layout>
       <h3>Requests</h3>
@@ -19,6 +30,19 @@ class RequestIndex extends Component {
         <Button primary>Add Request</Button>
         </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+            <HeaderCell>ID</HeaderCell>
+            <HeaderCell>Description</HeaderCell>
+            <HeaderCell>Amount</HeaderCell>
+            <HeaderCell>Recipient</HeaderCell>
+            <HeaderCell>Approval</HeaderCell>
+            <HeaderCell>Approve</HeaderCell>
+            <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+        </Table>
       </Layout>
     );
   }
--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -10,19 +10,48 @@ class RequestNew extends Component
     value: '',
     description: '',
     recipent: ''
+    loading: false,
+    errorMessage:''
   };
 
-  static async getInitialProps(props){
+  static async getInitialProps(props) {
     const { address } = props.query;
 
     return { address };
   }
 
+  onSubmit = async event => {
+    event.preventDefault();
+
+    const campaign = Campaign(this.state.address);
+    const { description, value, recipient } = this.state;
+
+    this.setState({ loading: true, errorMessage: ''});
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.createRequest(
+        description,
+        web.utils.toWei(value, 'ether'),
+        recipient
+      ).send({ from: accounts[0] });
+
+      Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message, });
+    }
+
+    this.setState({ loading: false, });
+  };
+
   render() {
     return (
     <Layout>
+    <Link route={`/campaigns/${this.props.address}/requests`}>
+    <a>Back</a>
+    </Link>
     <h3>Create a Request</h3>
-      <Form>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Description</label>
             <Input
@@ -50,7 +79,8 @@ class RequestNew extends Component
             />
           </Form.Field>
 
-          <Button primary>Create!</Button>
+          <Message error header="Oops!" content={this.state.errorMessage} />
+          <Button primary loading={this.state.loading}>Create!</Button>
         </Form>
       </Layout>
     );
