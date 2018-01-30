4aff17f78b23b0ba70e38d2265a00c4e44b03ba7
Gregory Kennedy <peacedude@gmail.com>
2018-01-30 05:06:24 -0800

    Add files via upload

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,10 @@
 import React, { Component } 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Grid, Button } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 import web3 from '../../ethereum/web3';
 import ContributeForm from '../../components/ContributeForm';
+import { Link } from '../../routes';
 
 
 class CampaignShow extends Component {
@@ -13,6 +14,7 @@ class CampaignShow extends Component {
     const summary = await campaign.methods.getSummary().call();
 
     return {
+      address: props.query.address,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
@@ -70,9 +72,29 @@ const {
     return (
       <Layout>
         <h3>Campaign Show</h3>
-        {this.renderCards()}
-        <ContributeForm />
-      </Layout>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column> width={10}>
+            {this.renderCards()}
+
+          </Grid.Column>
+
+          <Grid.Column> width={6}>
+            <ContributeForm address={this.props.address} />
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column>
+          <Link route={`/campaigns/${this.props.address}/requests`}>
+            <a>
+            <Button primary>View Requests</Button>
+          </a>
+        </Link>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Layout>
     );
   }
 }
